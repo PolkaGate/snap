9b6d3f82abd4ae20e376c394def7897d8768699e
Kami <ekbatanifard@gmail.com>
2024-06-22 02:23:42 +0330

    Update consts.ts

--- packages/dapp/src/util/consts.ts
+++ packages/dapp/src/util/consts.ts
@@ -1,6 +1,6 @@
 const SNAP_VERSION = '>=0.1.14';
 const LOCAL_SNAP_ID = 'local:http://localhost:8080';
-const NPM_SNAP_ID = 'npm:@polkagate/snap';
+// const NPM_SNAP_ID = 'npm:@polkagate/snap';
 
 export const DEFAULT_SNAP_ORIGIN =  LOCAL_SNAP_ID;
 
