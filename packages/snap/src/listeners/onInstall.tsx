808a6098335faf37bccefe5cb67ccdbc03914b82
Kami <ekbatanifard@gmail.com>
2024-12-03 10:57:55 +0330

    style: adjustment

--- packages/snap/src/listeners/onInstall.tsx
+++ packages/snap/src/listeners/onInstall.tsx
@@ -3,9 +3,7 @@
 
 import type { OnInstallHandler } from '@metamask/snaps-sdk';
 
-import { getKeyPair } from '../util';
 import { POLKADOT_GENESIS } from '@polkadot/apps-config';
-import { getLogoByGenesisHash } from '../ui/image/chains/getLogoByGenesisHash';
 import { setSnapState } from '../rpc/stateManagement';
 import { welcomeScreen } from '../ui/welcomeScreen';
 
@@ -18,16 +16,11 @@ import { welcomeScreen } from '../ui/welcomeScreen';
 export const onInstall: OnInstallHandler = async () => {
   setSnapState({ currentGenesisHash: POLKADOT_GENESIS }).catch(console.error);
 
-  const genesisHash = POLKADOT_GENESIS;
-  const { address } = await getKeyPair(undefined, genesisHash);
-  const logo = await getLogoByGenesisHash(genesisHash)
-
-
   await snap.request({
     method: 'snap_dialog',
     params: {
       type: 'alert',
-      content: welcomeScreen(address, genesisHash, logo)
+      content: welcomeScreen()
     },
   });
 };
