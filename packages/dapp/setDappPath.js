762ddb10bb92b0b0218cf33d252488553704cca8
Kami <ekbatanifard@gmail.com>
2024-12-08 17:56:14 +0330

    chore: remove console log

--- packages/dapp/setDappPath.js
+++ packages/dapp/setDappPath.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const packageJson = require('./package.json');
 
 const isProduction = process.env.NODE_ENV === 'production';
-console.log(`set path says isProduction is ${isProduction}`)
 packageJson.dependencies['@polkagate/extension-dapp'] = isProduction
   ? '^0.48.2'
   : '../../../polkadot-js-extension/packages/extension-dapp/build';
