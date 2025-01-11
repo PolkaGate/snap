f887c6f77303d076ef43e7132e778fa0ba8dd79f
Kami <ekbatanifard@gmail.com>
2025-01-11 15:48:31 +0330

    refactor: move WelcomeScreen component into on Install

--- packages/snap/src/listeners/onInstall.tsx
+++ packages/snap/src/listeners/onInstall.tsx
@@ -2,12 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { OnInstallHandler } from '@metamask/snaps-sdk';
+import { Box, Heading, Divider, Text, Bold, Link, Icon, Section, SnapComponent } from "@metamask/snaps-sdk/jsx";
 
 import { POLKADOT_GENESIS } from '@polkadot/apps-config';
 import { setSnapState } from '../rpc/stateManagement';
-import { welcomeScreen } from '../ui/welcomeScreen';
-
-
+import { UpdateInfo } from './onUpdate';
 
 /**
  * Handle installation of the snap. This handler is called when the snap is
@@ -20,7 +19,30 @@ export const onInstall: OnInstallHandler = async () => {
     method: 'snap_dialog',
     params: {
       type: 'alert',
-      content: welcomeScreen()
+      content: <WelcomeScreen />
     },
   });
+};
+
+
+const WelcomeScreen: SnapComponent = () => {
+  console.info(`Let's show welcome screen ...`)
+
+  return (
+    <Box >
+      <Heading>Welcome to Polkadot eco.! ✋</Heading>
+      <Divider />
+      <Text color='muted'>
+        Explore features like managing balances, staking, voting in governance, and more—all from the <Link href="metamask://snap/npm:@polkagate/snap/home">PolkaGate Snap Home</Link>.
+      </Text>
+      <Section>
+        <Text>To visit Home, open MetaMask, go to</Text>
+        <Box direction='horizontal' alignment='start'>
+          <Icon size='md' name='more-vertical' />
+          <Bold>Menu → Snaps</Bold>
+        </Box>
+        <Text>Then click on PolkaGate logo.</Text>
+      </Section>
+    </Box>
+  );
 };
