578b0e507bdbbbd6098e16d264e1235e7ff86a14
Kami <ekbatanifard@gmail.com>
2025-03-20 18:48:15 +0330

    chore: show balance refresh button by default

--- packages/snap/src/ui/home/partials/accountDemo.tsx
+++ packages/snap/src/ui/home/partials/accountDemo.tsx
@@ -56,11 +56,9 @@ export const accountDemo = (
       <Box alignment='space-between' direction='horizontal'>
         <Heading>Tokens</Heading>
         <Box center direction='horizontal'>
-          {!!nonZeroBalances?.length &&
-            <Button name='refreshBalances' variant='primary' >
-              <Icon size='md' color='primary' name='refresh' />
-            </Button >
-          }
+          <Button name='refreshBalances' variant='primary' >
+            <Icon size='md' color='primary' name='refresh' />
+          </Button >
           {!!nonZeroBalances?.length &&
             <Button name='balanceDetails' variant='primary' >
               <Icon size='md' color='primary' name={showDetails ? 'arrow-up' : 'arrow-down'} />
