6e5e607c441f0b8b40d9cef90f7615ab10eba585
Kami <ekbatanifard@gmail.com>
2025-03-25 21:44:56 +0330

    fix: sort logos along with chain names

--- packages/snap/src/ui/selectChains/CustomizeChains.tsx
+++ packages/snap/src/ui/selectChains/CustomizeChains.tsx
@@ -1,16 +1,16 @@
 import { Image, Box, Button, Container, Footer, Heading, Icon, Section, Text, Checkbox, Form } from "@metamask/snaps-sdk/jsx";
-import { getChainOptions, Options } from "../../chains";
+import { getChainOptions } from "../../chains";
 import { getLogoByGenesisHash } from "../image/chains/getLogoByGenesisHash";
 import type { HexString } from "@polkadot/util/types";
 import { getSnapState } from "../../rpc/stateManagement";
 import { DEFAULT_CHAINS_GENESIS } from "../../constants";
 
-const ui = (options: Options[], logoList: string[], selectedChains: HexString[]) => {
+const ui = (chains: {  text: string; value: string;  logo: string;}[], selectedChains: HexString[]) => {
 
-  const sortedOptions = options.sort(({ value: a }, { value: b }) => {
+  const sortedChains = chains.sort(({ value: a }, { value: b }) => {
     const isCheckedA = selectedChains.includes(a as HexString);
     const isCheckedB = selectedChains.includes(b as HexString);
-  
+
     return Number(isCheckedB) - Number(isCheckedA);
   });
 
@@ -33,12 +33,12 @@ const ui = (options: Options[], logoList: string[], selectedChains: HexString[])
           </Box>
         </Box>
         <Form name='selectedChains'>
-          {sortedOptions.map(({ text, value }, index) => {
+          {sortedChains.map(({ text, value, logo }, index) => {
             return (
               <Section>
                 <Box direction='horizontal' alignment='space-between' center>
                   <Box direction='horizontal' alignment='start' center>
-                    <Image src={logoList[index]} />
+                    <Image src={logo} />
                     <Text alignment='start'>
                       {text}
                     </Text>
@@ -61,14 +61,17 @@ const ui = (options: Options[], logoList: string[], selectedChains: HexString[])
 
 export async function CustomizeChains(id: string) {
   const options = getChainOptions()
-  const logoList = await Promise.all(options.map(({ value }) => getLogoByGenesisHash(value as HexString)));
+  const chains = await Promise.all(options.map(async({ text, value }) => {
+    return { text,value, logo: await getLogoByGenesisHash(value as HexString) }
+  }));
+
   const selectedChains = (await getSnapState('selectedChains')) || DEFAULT_CHAINS_GENESIS;
 
   await snap.request({
     method: 'snap_updateInterface',
     params: {
       id,
-      ui: ui(options, logoList, selectedChains)
+      ui: ui(chains, selectedChains)
     },
   });
 }
