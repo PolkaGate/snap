d4c3225ad8e1b14e925d10ce3ea28c4823663106
Kami <ekbatanifard@gmail.com>
2025-09-29 16:24:23 +0330

    chore: bump packages

--- packages/snap/snap.config.ts
+++ packages/snap/snap.config.ts
@@ -2,7 +2,6 @@ import { resolve } from 'path';
 import type { SnapConfig } from '@metamask/snaps-cli';
 
 const config: SnapConfig = {
-  bundler: 'webpack',
   input: resolve(__dirname, 'src/index.tsx'),
   server: {
     port: 8080,
